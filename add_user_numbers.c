7cb2312c0f03bdb568766531af08fd5e4de7148c
repl.it user <replituser@example.com>
2020-11-01 00:39:38 +0000

    clean up output for add_user_numbers

--- add_user_numbers.c
+++ add_user_numbers.c
@@ -5,13 +5,13 @@ int main(void) {
 
    // gets asks user for string
    int first_number; int second_number; 
-   printf("Please enter your first number >> \t");
+   printf("Please enter your first number: \t");
    scanf("%d", &first_number); 
    printf("Your first number is %d\n", first_number); 
-   printf("Please enter your second number >> \t");
+   printf("Please enter your second number: \t");
    scanf("%d", &second_number); 
-   printf("Your second number is %d\n", second_number); 
+   printf("Your second number is %d\n\n", second_number); 
    // int answer = first_number + second_number;
-   printf("%i + %i = %i\n", first_number, second_number, first_number + second_number);
+   printf("--\t%i + %i = %i\t--\n", first_number, second_number, first_number + second_number);
    return 0; 
 }
